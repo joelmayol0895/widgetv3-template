e00c44dd64c0e0863c6451e70fd6a6dc4a572f40
Joel Mayol <joelmayol0895@gmail.com>
2025-03-04 14:32:05 +0800

    adding back the widget

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "";
+  let InstanceId_Widget = "513953";
   let InstanceId_QA = "513951";
   let InstanceId_SEO = "600662";
   let InstanceId_Carousel = "600665";
